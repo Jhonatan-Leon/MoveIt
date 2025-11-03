bb49b440d596b931abc09ce44041d55d2e0b19c3
Jhonatan-Leon <c.jhonatan2007@gmail.com>
2025-11-03 13:59:33 -0500

    feat: Crear PackageFacade con patrón Singleton y métodos CRUD delegados en PackageService

---
+++ edu/co/Facade/PackageFacade.java
@@ -0,0 +1,35 @@
+package edu.co.Facade;
+
+import edu.co.Errors.ControllException;
+import edu.co.Services.PackageService;
+import edu.co.Model.Package;
+import java.util.List;
+
+public class PackageFacade {
+    private static PackageFacade instance;
+
+    public PackageFacade getInstance(){
+        if(instance == null){
+            instance = new PackageFacade();
+        }
+        return instance;
+    }
+
+    public List<Package> getAllPackages() throws ControllException.ErrorServer{
+        return PackageService.getAllPackage();
+    }
+
+    public boolean AddPackage(Package newPackage) throws ControllException.PackageCreate{
+        return PackageService.AddPackage(newPackage);
+    }
+
+    public Package updatePackage(Package updatePackage) throws ControllException.ErrorServer{
+        return PackageService.updatePackage(updatePackage);
+    }
+
+    public boolean deletePackage(String Id) throws ControllException.ErrorServer{
+        return PackageService.DeletePackage(Id);
+    }
+
+
+}
