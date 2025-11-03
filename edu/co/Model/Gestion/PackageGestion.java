93c0231b51988935de181c1f78148d4d3546f215
Jhonatan-Leon <c.jhonatan2007@gmail.com>
2025-11-03 13:59:40 -0500

    feat: Crear PackageGestion con patrón Singleton y métodos CRUD para gestionar paquetes

---
+++ edu/co/Model/Gestion/PackageGestion.java
@@ -0,0 +1,56 @@
+package edu.co.Model.Gestion;
+
+import edu.co.Model.Package;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PackageGestion {
+    private static PackageGestion Instance;
+    private static final List<Package> listPackage = new ArrayList<>();
+
+    public static PackageGestion getInstance() {
+        if(Instance == null){
+            return new PackageGestion();
+        }
+        return Instance;
+    }
+
+    private PackageGestion() {
+    }
+
+    public List<Package> getAllPackages(){
+        return new ArrayList<>(listPackage);
+    }
+
+    public boolean AddPackage(Package newPackage){
+        listPackage.add(newPackage);
+        return true;
+    }
+
+    public Package updatePackage(Package updatePackage){
+        for(Package p : listPackage) {
+            if(p.equals(updatePackage)){
+                p.setPeso(updatePackage.getPeso());
+                p.setAlto(updatePackage.getAlto());
+                p.setAncho(updatePackage.getAncho());
+                p.setVolumen(updatePackage.getVolumen());
+            }
+        }
+
+        return null;
+    }
+
+
+    public boolean deletePackage(Package p){
+        return listPackage.remove(p);
+    }
+
+    public Package getPackageById(String Id){
+        for(Package p: listPackage){
+            if(p.getIdPaquete().equals(Id)){
+                return p;
+            }
+        }
+        return null;
+    }
+}
