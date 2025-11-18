ac9080a1ff99114a56495637def9dac76f1c59df
Jhonatan-Leon <c.jhonatan2007@gmail.com>
2025-11-17 19:31:07 -0500

    feat: agregar subclases de excepciones específicas con códigos de estado para mejorar manejo de errores

--- src/main/java/uniquindio/Errors/ControllException.java
+++ src/main/java/uniquindio/Errors/ControllException.java
@@ -3,60 +3,72 @@ package uniquindio.Errors;
 import uniquindio.Model.AplicationException;
 
 public class ControllException {
-    private ControllException(){
+    private ControllException() {
     }
 
-    public static class ErrorServer extends AplicationException{
-        public ErrorServer(String message){
-            super(message);
+    public static class ErrorServer extends AplicationException {
+        public ErrorServer(String message) {
+            super(message, 500);
         }
     }
 
     public static class UserNotFound extends AplicationException {
-        public UserNotFound(String message){
-            super(message);
+        public UserNotFound(String message) {
+            super(message, 404);
         }
     }
 
     public static class UserUpdate extends AplicationException {
-        public UserUpdate(String message){
-            super(message);
+        public UserUpdate(String message) {
+            super(message, 400);
         }
     }
 
     public static class UserDelete extends AplicationException {
-        public UserDelete(String message){
-            super(message);
+        public UserDelete(String message) {
+            super(message, 400);
         }
     }
 
     public static class UserCreate extends AplicationException {
-        public UserCreate(String message){
-            super(message);
+        public UserCreate(String message) {
+            super(message, 400);
         }
     }
 
     public static class PackageNotFound extends AplicationException {
-        public PackageNotFound(String message, Throwable cause){
-            super(message);
+        public PackageNotFound(String message) {
+            super(message, 404);
         }
     }
 
-    public static class PackageUpdate extends AplicationException {
-        public PackageUpdate(String message){
-            super(message);
-        }
-    }
-
-    public static class PackageDelete extends AplicationException {
-        public PackageDelete(String message){
-            super(message);
-        }
-    }
     public static class PackageCreate extends AplicationException {
-        public PackageCreate(String message){
-            super(message);
+        public PackageCreate(String message) {
+            super(message, 400);
         }
     }
 
+    public static class EnvioNotFound extends AplicationException {
+        public EnvioNotFound(String message) {
+            super(message, 404);
+        }
+    }
+
+    public static class EnvioCreate extends AplicationException {
+        public EnvioCreate(String message) {
+            super(message, 400);
+        }
+    }
+
+    public static class CotizacionInvalid extends AplicationException {
+        public CotizacionInvalid(String message) {
+            super(message, 400);
+        }
+    }
+
+    public static class TarifaError extends AplicationException {
+        public TarifaError(String message) {
+            super(message, 500);
+        }
+    }
 }
